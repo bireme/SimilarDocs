dddd3fe08f80e4e5eb3ec2c863c6721b425ddc00
Heitor <heitor@bir927595.wdc.paho.org>
2017-05-26 11:54:09 -0300

    debuging

--- SDService/src/java/org/bireme/sds/SDService.java
+++ SDService/src/java/org/bireme/sds/SDService.java
@@ -77,8 +77,8 @@ public class SDService extends HttpServlet {
         updaterService = new UpdaterService(docIndex, Conf.idxFldNames());
 
         context.setAttribute("MAINTENANCE_MODE", Boolean.FALSE);
-        System.out.println("I will call 'updaterService.start()'");
-        updaterService.start();
+        //System.out.println("I will call 'updaterService.start()'");
+        //updaterService.start();
     }
     
     @Override
