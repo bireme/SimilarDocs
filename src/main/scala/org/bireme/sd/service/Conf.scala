c67be6a671fb1f9c007fa2fa9d31be62c67a0035
Heitor <heitor@bir927595.wdc.paho.org>
2017-09-12 14:25:30 -0300

    decs field is now used with search

--- src/main/scala/org/bireme/sd/service/Conf.scala
+++ src/main/scala/org/bireme/sd/service/Conf.scala
@@ -30,5 +30,5 @@ object Conf {
   val idxFldNames = Set("ti","ti_pt","ti_ru","ti_fr","ti_de","ti_it","ti_en",
                        "ti_es","ti_eng","ti_Pt","ti_Ru","ti_Fr","ti_De","ti_It",
                        "ti_En","ti_Es","ab_en","ab_es","ab_Es","ab_de","ab_De",
-                       "ab_pt","ab_fr","ab_french", "ab")
+                       "ab_pt","ab_fr","ab_french", "ab", "decs")
 }
